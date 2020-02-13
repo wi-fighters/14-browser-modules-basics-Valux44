4725ad7aa92d7fedd017065745da620cd0d247c5
Val <valerysehbai@gmail.com>
2020-02-13 11:36:24 +0100

    done

---
+++ modules/index.js
@@ -0,0 +1,7 @@
+import { scrollWindow } from "/modules/scroll.js";
+import { clickImages } from "/modules/click.js";
+import { hoverHeader } from "/modules/hover.js";
+
+scrollWindow();
+clickImages();
+hoverHeader();
